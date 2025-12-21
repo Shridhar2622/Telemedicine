a0b34bd176d3f68cb17587cc93a2dc33059b1338
Shridhar Pandey <panshri248@gmail.com>
2025-12-21 17:30:38 +0530

    depploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5175", "http://localhost:5174", process.env.FRONTEND_URL].filter(Boolean),
+    origin: ["http://localhost:5173", "http://localhost:5175", "http://localhost:5174", "https://celebrated-semifreddo-915145.netlify.app", process.env.FRONTEND_URL].filter(Boolean),
     credentials: true,
   })
 );
--- backend/src/socket.js
+++ backend/src/socket.js
@@ -5,7 +5,7 @@ let io;
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:5175", "http://localhost:5174", process.env.FRONTEND_URL].filter(Boolean),
+      origin: ["http://localhost:5173", "http://localhost:5175", "http://localhost:5174", "https://celebrated-semifreddo-915145.netlify.app", process.env.FRONTEND_URL].filter(Boolean),
       methods: ["GET", "POST"],
       credentials: true
     },
